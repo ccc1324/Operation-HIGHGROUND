cdfded35e4c43086d6b8a743cf89e2c9eb658eb5
mekkirachedine <mekkirachedine1@gmail.com>
2019-02-04 17:42:19 -0600

    2P controls

--- Assets/Resources/Scripts/shooter.cs
+++ Assets/Resources/Scripts/shooter.cs
@@ -26,12 +26,13 @@ public class shooter : MonoBehaviour
     public void Shoot()
     {
         //Controls for mouse (somewhat buggy)
-        //Vector3 direction = Input.mousePosition - _cam.WorldToScreenPoint(_holder.position);
-        //float angle = Mathf.Atan2(direction.y, direction.x) * 360 / (2 * Mathf.PI);
+        Vector3 direction = Input.mousePosition - _cam.WorldToScreenPoint(_holder.position);
+        float angle = Mathf.Atan2(direction.y, direction.x) * 360 / (2 * Mathf.PI);
 
-        float xaxis = Input.GetAxis("Horizontal");
-        float yaxis = Input.GetAxis("Vertical");
-        float angle = Mathf.Atan2(yaxis, xaxis) * 360 / (2 * Mathf.PI);
+        //float xaxis = Input.GetAxis("Horizontal");
+        //float yaxis = Input.GetAxis("Vertical");
+        //float angle = Mathf.Atan2(yaxis, xaxis) * 360 / (2 * Mathf.PI);
+
         Quaternion rotation = Quaternion.Euler(0, 0, angle);
         GameObject bullet = Instantiate(BulletPrefab, _holder.position, rotation, _holder);
     }
