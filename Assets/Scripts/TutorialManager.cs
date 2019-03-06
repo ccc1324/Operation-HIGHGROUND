b0451f2b57a09268a95edcb9ee401ad86f880f3d
Calvin <43643839+bot1324@users.noreply.github.com>
2019-03-06 12:40:47 -0600

    Started on Tutorial Level

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject Player1;
+    public Camera Camera;
+    private float _leader_height;
+    private float _camera_height;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //Camera Management
+        _leader_height = Player1.transform.position.y;
+        _camera_height = Camera.transform.position.y;
+        if (_leader_height > _camera_height + Camera.orthographicSize * 0.5f) //leader height should be <= 3/4 of camera height
+            Camera.transform.position = new Vector3(0, _leader_height - Camera.orthographicSize * 0.5f, -10f);
+        if (_leader_height < _camera_height) //leader height should be >= 1/2 of camera height
+            Camera.transform.position = new Vector3(0, _leader_height, -10f);
+    }
+}
